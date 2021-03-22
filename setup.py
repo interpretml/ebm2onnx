a5299bad448d246c6930923dd58625088975a976
Romain Picard <romain.picard@softathome.com>
2021-03-22 19:06:59 +0100

    Bump version: 1.0.0 → 1.0.0

--- setup.py
+++ setup.py
@@ -36,6 +36,6 @@ setup(
     name='ebm2onnx',
     packages=find_packages(include=['ebm2onnx']),
     url='https://github.com/SoftAtHome/ebm2onnx.git',
-    version='1.0.0',
+    version='version='1.0.0'',
     zip_safe=True,
 )
