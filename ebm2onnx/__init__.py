c173c0e21731730b9c08727b5f791f25a97c24bf
Romain Picard <romain.picard@softathome.com>
2021-03-22 19:07:01 +0100

    Bump version: 1.0.0 → 1.0.1

--- ebm2onnx/__init__.py
+++ ebm2onnx/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Romain Picard"""
 __email__ = 'romain.picard@softathome.com'
-__version__ = '__version__ = '1.0.0''
+__version__ = '__version__ = '1.0.1''
 
 from .convert import to_onnx
