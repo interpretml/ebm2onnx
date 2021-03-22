d9116515b915e63a4cf80946f19b04ebcd04b70a
Romain Picard <romain.picard@softathome.com>
2021-03-22 18:45:35 +0100

    update classifiers

--- setup.py
+++ setup.py
@@ -18,14 +18,14 @@ requirements = [
 setup(
     author="Romain Picard",
     author_email='romain.picard@softathome.com',
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
