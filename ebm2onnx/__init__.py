898826b412e40f2a90c275b3a9a11f1106817825
Romain Picard <romain.picard@softathome.com>
2025-12-18 14:35:47 +0100

    Bump version: 3.3.0 → 3.3.1
    Signed-off-by: Romain Picard <romain.picard@softathome.com>

--- ebm2onnx/__init__.py
+++ ebm2onnx/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Romain Picard"""
 __email__ = 'romain.picard@softathome.com'
-__version__ = '3.3.0'
+__version__ = '3.3.1'
 
 from .convert import to_onnx, get_dtype_from_pandas
 from . import sklearn
